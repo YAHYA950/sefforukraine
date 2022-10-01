cec3fcbbd8266ed6397a222805b308bbf137dd1d
Yahya <simplemoodyperson@gmail.com>
2022-10-01 21:28:33 +0500

    make it better

---
+++ Account/Register.php
@@ -0,0 +1,408 @@
+<!DOCTYPE html>
+
+
+
+<!-- Mirrored from sefforukraine.sef.pt/Account/Register.aspx by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 23 Sep 2022 16:42:07 GMT -->
+<!-- Added by HTTrack -->
+<!-- Mirrored from sefforukraine.sef.net.in/Account/Register.php by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 01 Oct 2022 13:35:48 GMT -->
+<!-- Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+<html>
+
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>
+        New user registration
+    </title>
+    <link href="../Content/fontawesome.min.css" rel="stylesheet" />
+    <link href="../Content/myFooter.css" rel="stylesheet" />
+    <link href="../Content/bootstrap.css" rel="stylesheet" />
+    <link href="../Content/Site.css" rel="stylesheet" />
+    <script src="../Scripts/jquery-3.6.0.js"></script>
+    <script src="../Scripts/bootstrap.js"></script>
+    <script src="../Scripts/inputmask/jquery.inputmask.js"></script>
+    <script src="../Scripts/fontawesome/all.min.js"></script>
+
+
+
+    <style type="text/css">
+        .navbar-blue {
+            background-color: rgb(27 66 110/0.8);
+        }
+
+        #imgFlag {
+            max-height: 60px;
+            max-width: 60px;
+        }
+
+        .bg-image {
+            background-image: url('../Images/bg.png');
+            background-color: aliceblue;
+            background-attachment: fixed;
+            background-repeat: no-repeat;
+            background-position-x: center;
+            background-position-y: center;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }
+
+
+
+        /*  .bgf{
+            background-image :url('/images/flags/pt.png')
+        }
+           .ddlstyle
+        {
+            width: 200px;
+        }
+        option
+        {
+            padding-left: 50px;
+            margin-left: 20px;
+            font-size: 12px;
+        }*/
+    </style>
+
+</head>
+
+
+<body class="d-flex flex-column h-100  bg-image ">
+
+
+    <script type="text/javascript">
+        //<![CDATA[
+        var theForm = document.forms['ctl01'];
+        if (!theForm) {
+            theForm = document.ctl01;
+        }
+
+        function __doPostBack(eventTarget, eventArgument) {
+            if (!theForm.onsubmit || (theForm.onsubmit() != false)) {
+                theForm.__EVENTTARGET.value = eventTarget;
+                theForm.__EVENTARGUMENT.value = eventArgument;
+                theForm.submit();
+            }
+        }
+        //]]>
+    </script>
+
+
+    <script src="../WebResource4f864f86.js?d=pynGkmcFUV13He1Qd6_TZEZ4Zukm5DdMrx0tz4A4lV7cCXB5Q_8_mHv7rTKHQOqD6tVN1Y3n2xljCDpttmAmiw2&amp;t=637353014110221559" type="text/javascript"></script>
+
+
+    <script src="../ScriptResourcedc4edc4e.js?d=NJmAwtEo3Ipnlaxl6CMhvlxcP23hdSSGPbMy6XVb3I32ZuVd9QlSyuM51502DLR0ti7JyWBRxytGm6TZXFJ8DJ81WUJrkUWcV9EIvavpKsNaQJRU1rrO4n5K1XVfih11UVvmalh96DN_BZxdqDwRRtCQgWPzggwVq7befaQTvnU1&amp;t=ffffffffdfc97409" type="text/javascript"></script>
+    <script src="../ScriptResource33273327.js?d=dwY9oWetJoJoVpgL6Zq8OFcKkugwwN_XMSVookNTScpGj9WWqmxQJ9g8QlYpQRyhCvhpwt7k3pTDK4Jf_26SG4Ed-raM_QqWE1F_VI7nGTO-SrqVxLXfks95t2XhjdqnGkqNLv6wBliOG2lf8kJcPTAD6P9H11t_A66EQviA82s1&amp;t=ffffffffdfc97409" type="text/javascript"></script>
+    <script src="../WebResourcedf1cdf1c.js?d=JoBkLzP19aTuxbWOhHobYpab9XWv-6g214ikOCZ1QWxxoZNx3UBpzPWoeBlOjiBsz0BlSs8RXP_OCiUoSOJrUw2&amp;t=637353014110221559" type="text/javascript"></script>
+    <div class="aspNetHidden">
+
+        <input type="hidden" name="__VIEWSTATEGENERATOR" id="__VIEWSTATEGENERATOR" value="87EDC3EC" />
+        <input type="hidden" name="__EVENTVALIDATION" id="__EVENTVALIDATION" value="AioSDvyXSD0P36OAHEDFertFXWmVqcoq5uEqA3bXPtU4vuOVfYd2TVh4Fi5gxumyTVfaw0PYdxLQ3EQmELt1uHRUcrFjV/5lXJnN0qw7XXw8/D/ETtiYE5L6wXGKUEasRvNq2FWDAk0GvL3tMs7HzUEh5a7Lmph9WshC5w1cXBgEOaClpOG+hfj/Zc+z/7qc8JeQRfOMrsOi6EIIiL7squ3pd9miSg/oRKQfHexKqOg=" />
+    </div>
+
+
+
+    <header class="fixed-top navbar-blue ps-1  ">
+        <div class="container">
+            <div class="row py-2 ">
+                <div class="col-4">
+                    <a href="../Default.html" class="float-start">
+                        <img class="img-fluid " src='../Images/Logo.png' /></a>
+                </div>
+
+                <div class="col-8">
+
+                    <div class="float-end LogInV clearfix">
+
+                        <a href="Register.php" role="menuitem" class="p-2 link-light ">Register</a>
+                        <a href="Login.php" role="menuitem" class="link-light p-2">Authenticate </a>
+
+
+
+
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+
+
+    </header>
+    <script type="text/javascript">
+        //<![CDATA[
+        Sys.WebForms.PageRequestManager._initialize('ctl00$ctl09', 'ctl01', ['tctl00$MainContent$UpdatePanel1', 'MainContent_UpdatePanel1'], [], [], 90, 'ctl00');
+        //]]>
+    </script>
+
+
+
+    <main role="main" class="flex-shrink-0  mb-5 " style="margin-top: 100px;">
+        <div class="container">
+
+
+            <style type="text/css">
+                .containerAU {
+                    max-width: 650px;
+                }
+            </style>
+
+            <div id="MainContent_UpdatePanel1">
+
+
+                <div class="containerAU drop-shadow container pt-1 pb-1 ">
+                    <div class="card mt-3 mb-3 ">
+                        <div class="card-header bg-primary text-white">
+                            <h5>
+                                <span id="MainContent_lblHeader">Create an account</span>
+                            </h5>
+                        </div>
+                        <div class="card-body">
+                            <span id="MainContent_Mensagem"></span>
+                            <div class="form-horizontal">
+
+                                <div class="alert alert-info" style="display: none">
+                                    <span id="MainContent_lblSubHeader">Please use a personal email, not a shared email account.</span>
+                                </div>
+
+                                <form method="post" name="signup" onsubmit="return checkpass();">
+
+                                    <div class="row mb-3">
+                                        <div class="col-md-6">
+                                            <div class="form-floating mb-3 mb-md-0">
+                                                <input class="form-control" id="fname" name="fname" type="text" placeholder="Enter your first name" required />
+                                                <label for="inputFirstName">First name</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-floating">
+                                                <input class="form-control" id="lname" name="lname" type="text" placeholder="Enter your last name" required />
+                                                <label for="inputLastName">Last name</label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="row mb-3">
+                                        <div class="col-md-6">
+                                            <div class="form-floating mb-3 mb-md-0">
+                                                <input class="form-control" id="N" name="N" type="text" placeholder="Nationality" required />
+                                                <label for="inputFirstName">Nationality</label>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-6">
+                                            <div class="form-floating">
+                                                <input class="form-control" id="SEX" name="SEX" type="text" placeholder="SEX" required />
+                                                <label for="inputLastName">SEX</label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row mb-3">
+                                        <div class="col-md-6">
+                                            <div class="form-floating mb-3 mb-md-0">
+                                                <input class="form-control" id="BD" name="BD" type="text" placeholder="DD/MM/YY" required />
+                                                <label for="inputFirstName">Birthdate</label>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-6">
+                                            <div class="form-floating">
+                                                <input class="form-control" id="TIN" name="TIN" type="text" placeholder="TAX Id Number(NIF)" required />
+                                                <label for="inputLastName">TAX Id Number(NIF)</label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+
+                                    <div class="row mb-3">
+                                        <div class="col-md-6">
+                                            <div class="form-floating mb-3 mb-md-0">
+                                                <input class="form-control" id="SSN" name="SSN" type="text" placeholder="Social Security Number(NISS)" />
+                                                <label for="inputFirstName">Social Security Number(NISS)</label>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-6">
+                                            <div class="form-floating">
+                                                <input class="form-control" id="HSN" name="HSN" type="text" placeholder="Health System Number(NUS)" required />
+                                                <label for="inputLastName">Health System Number(NUS)</label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+
+
+
+
+
+                                    <div class="form-floating mb-3">
+                                        <input class="form-control" id="email" name="email" type="email" placeholder="phpgurukulteam@gmail.com" required />
+                                        <label for="inputEmail">Email address</label>
+                                    </div>
+
+
+                                    <div class="form-floating mb-3">
+                                        <input class="form-control" id="contact" name="contact" type="text" placeholder="1234567890" required pattern="[0-9]{10}" title="10 numeric characters only" required />
+                                        <label for="inputcontact">Contact Number</label>
+                                    </div>
+
+
+
+                                    <div class="row mb-3">
+                                        <div class="col-md-6">
+                                            <div class="form-floating mb-3 mb-md-0">
+                                                <input class="form-control" id="password" name="password" type="password" placeholder="Create a password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" title="at least one number and one uppercase and lowercase letter, and at least 6 or more characters" required />
+                                                <label for="inputPassword">Password</label>
+                                            </div>
+                                        </div>
+
+
+                                        <div class="col-md-6">
+                                            <div class="form-floating mb-3 mb-md-0">
+                                                <input class="form-control" id="confirmpassword" name="confirmpassword" type="password" placeholder="Confirm password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" title="at least one number and one uppercase and lowercase letter, and at least 6 or more characters" required />
+                                                <label for="inputPasswordConfirm">Confirm Password</label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="mt-4 mb-0">
+                                        <div class="d-grid"><button type="submit" class="btn btn-primary btn-block" name="submit">Create Account</button></div>
+                                    </div>
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <script src="../www.google.com/recaptcha/api8c9b8c9b.html?onload=renderRecaptcha&amp;render=explicit" async defer></script>
+
+
+                <script type="text/javascript">
+                    var your_site_key = '6LcJPTUUAAAAAApYt0_OWtPKizfRIlefYYoLhl1G';
+                    var renderRecaptcha = function() {
+                        grecaptcha.render('ReCaptchContainer', {
+                            'sitekey': your_site_key,
+                            'callback': reCaptchaCallback,
+                            theme: 'light',
+                            type: 'image',
+                            size: 'normal'
+
+                        });
+
+                    };
+
+                    var reCaptchaCallback = function(response) {
+                        if (response !== '') {
+                            jQuery('#lblMessage').css('color', 'green').html('Successo!');
+
+                        }
+
+                    };
+                    jQuery('button[type="button"]').click(function(e) {
+                        var message = 'Click na checkbox';
+                        if (typeof(grecaptcha) != 'undefined') {
+                            var response = grecaptcha.getResponse();
+                            (response.length === 0) ? (message = 'Verificação mal sucedida!') : (message = 'Successo!');
+
+                        }
+                        jQuery('#lblMessage').html(message);
+                        jQuery('#lblMessage').css('color', (message.toLowerCase() == 'sucesso!') ? "green" : "red");
+
+                    });
+                </script>
+
+
+
+            </div>
+
+
+        </div>
+    </main>
+
+
+    <div style="margin-bottom: 80px;">
+        <div class="footer-basic fixed-bottom mt-5 ">
+            <footer>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-4">
+                            <p class="copyright">SEF © All rights reserved</p>
+                        </div>
+                        <div class="col-4">
+
+
+                            <div class="social">
+                                <a href="https://www.facebook.com/servicodeestrangeirosefronteiras" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                                <a href="https://twitter.com/sef_portugal" target="_blank"><i class="fab fa-twitter"></i></a>
+                                <a href="https://www.youtube.com/servicodeestrangeirosefronteiras" target="_blank"><i class="fab fa-youtube"></i></a>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="float-end">
+                                <p class="copyright "><a href="../doc/SEF_for_Ukraine_Politica_Privacidade_e_Cookies_EN.pdf" target="_blank" class="text-white">Privacy Policy and Cookies</a> </p>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                </div>
+
+            </footer>
+
+
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+    <script type="text/javascript">
+        //<![CDATA[
+        WebForm_AutoFocus('MainContent_Email'); //]]>
+    </script>
+    </form>
+
+    <script src="../Scripts/bootstrap.js"></script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-48782000-9"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-48782000-9');
+    </script>
+
+
+</body>
+
+
+
+
+<!-- Mirrored from sefforukraine.sef.pt/Account/Register.aspx by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 23 Sep 2022 16:42:10 GMT -->
+
+<!-- Mirrored from sefforukraine.sef.net.in/Account/Register.php by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 01 Oct 2022 13:35:49 GMT -->
+
+</html>
